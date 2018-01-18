80669655f54d35c43eb15fde5824ebf530cf632b
Ricardo Abreu <rj.abreu89@gmail.com>
2018-01-18 00:13:48 +0000

    storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,20 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import globalStyles from "./globalStyles";
+
+import React from "react";
+
+globalStyles();
+
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "200px"
+};
+
+//decorators
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
+addDecorator(CenterDecorator);
 
 const req = require.context("../src/components", true, /\.stories\.js$/);
 
