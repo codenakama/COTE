781b98b5d64850d9c72db8f611a33b7af74afc9d
Ricardo Abreu <rj.abreu89@gmail.com>
2018-01-22 10:45:41 +0000

    paper component

---
+++ src/components/Paper/Paper.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const PaperWrapper = styled.div`
+  min-width: 150px;
+  min-height: 100px;
+  border-radius: 4px;
+  background-color: #ffffff;
+  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14);
+  padding: 1rem;
+`;
+
+const Paper = props => {
+  const { children } = props;
+  return <PaperWrapper>{children}</PaperWrapper>;
+};
+
+Paper.propTypes = {};
+
+export default Paper;
---
+++ src/components/Paper/Paper.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Paper from "./Paper";
+
+storiesOf("Paper", module).add("Normal", () => (
+  <Paper>SOme elements inside</Paper>
+));
