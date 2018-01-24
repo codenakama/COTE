464e337bcc61f43257f021c7805a0b66fb686157
Ricardo Abreu <rj.abreu89@gmail.com>
2018-01-24 17:41:03 +0000

    added docs for testing lib works

---
+++ docs/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, lato, initGlobalStyles } from "cote";
+
+initGlobalStyles();
+
+const App = () => (
+  <div>
+    <Button type="submit">Hello world</Button>
+  </div>
+);
+
+export default App;
