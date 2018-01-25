9e25ee68521687c3b057862aaefd9b81f06521c1
Ricardo Abreu <rj.abreu89@gmail.com>
2018-01-25 18:16:09 +0000

    fix wh

--- src/components/CotCard/CotCard.js
+++ src/components/CotCard/CotCard.js
@@ -142,9 +142,7 @@ const CotCard = ({ cardHolderName, ...props }) => {
   return (
     <div>
       <svg
-        width="320px"
-        height="204px"
-        viewBox="0 0 320 204"
+        viewBox="0 0 w h"
         version="1.1"
         xmlnsXlink="http://www.w3.org/1999/xlink"
       >
