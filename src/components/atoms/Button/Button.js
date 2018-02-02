90ea4f0c2a0ea211449a9b2e8edc0f1573b054d1
Ricardo Abreu <rj.abreu89@gmail.com>
2018-02-02 17:54:10 +0000

    button size mobile

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -59,6 +59,14 @@ export const StyledButton = styled.button`
       if (props.disabled) return;
       if (!props.solid) return "1";
     }};
+
+    @media and screen(max-width: 448px ) {
+      width: ${props => {
+        if (props.small) return "16px";
+        if (props.large) return "224px";
+        if (props.full) return "100%";
+      }};
+    }
   }
 
   border: ${props =>
