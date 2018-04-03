be45f442f61d35e0634f35af8de474dbb0497017
Ricardo Abreu <rj.abreu89@gmail.com>
2018-04-03 16:46:38 +0100

    change max width

--- src/components/CaptionedImageBox/index.js
+++ src/components/CaptionedImageBox/index.js
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
   border-radius: 4px;
   background-color: #ffffff;
   box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14);
-  max-width: 320px;
+  max-width: 360px;
   overflow: hidden;
 `;
 
