975b07b90f7e4597c9aa05b48fa0b7f18d564f6c
Ricardo Abreu <rj.abreu89@gmail.com>
2018-04-05 17:02:02 +0100

    allow max width to nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { StyledLinkButton } from '../atoms/Button/Button';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { StyledLinkButton } from "../atoms/Button/Button";
 
 const NavButton = StyledLinkButton.extend`
   @media screen and (max-width: 640px) {
@@ -23,7 +23,14 @@ const Nav = styled.nav`
   height: 72px;
   padding: 0 3.43em;
   background-color: #fff;
-  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14);
+  max-width: ${props => (props.maxWidth ? props.maxWidth : "928px")};
+  margin: 0 auto;
+  @media only screen and (max-width: 960px) {
+    margin: 0 40px;
+  }
+  @media only screen and (max-width: 40em) {
+    margin: 0 1em;
+  }
 
   @media screen and (max-width: 640px) {
     height: 56px;
@@ -70,15 +77,15 @@ Navigation.propTypes = {
 };
 
 Navigation.defaultProps = {
-  logo: 'http://via.placeholder.com/222x40',
+  logo: "http://via.placeholder.com/222x40",
   items: [
     {
-      url: 'http://google.com',
-      title: 'Nav item 1'
+      url: "http://google.com",
+      title: "Nav item 1"
     },
     {
-      url: 'http://google.com',
-      title: 'Nav item 2'
+      url: "http://google.com",
+      title: "Nav item 2"
     }
   ]
 };
