1325d0c3df3153f2c9594201de67d45ebeb3a449
Ricardo Abreu <rj.abreu89@gmail.com>
2018-04-25 10:11:48 +0100

    white-space nowrap on button

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,8 +1,8 @@
-import styled, { keyframes } from 'styled-components';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { colors } from '../../../styles/defaults';
-import Icon from '../Icon/Icon';
+import styled, { keyframes } from "styled-components";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { colors } from "../../../styles/defaults";
+import Icon from "../Icon/Icon";
 
 const boxShadow = `box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 transition: all 0.3s cubic-bezier(.25,.8,.25,1);`;
@@ -12,6 +12,7 @@ export const StyledButton = styled.button`
   height: 38px;
   font-family: inherit;
   text-decoration: none;
+  white-space: nowrap;
   background-color: ${props => {
     if (props.disabled) return colors.darkGrey;
     if (props.danger && props.theme) return props.theme.colorDanger;
@@ -20,11 +21,11 @@ export const StyledButton = styled.button`
 
     if (props.theme && props.solid) return props.theme.colorPrimary;
 
-    return 'transparent';
+    return "transparent";
   }};
 
   color: ${props => {
-    if (props.solid || props.disabled) return '#fff';
+    if (props.solid || props.disabled) return "#fff";
 
     return colors.black;
   }};
@@ -36,13 +37,13 @@ export const StyledButton = styled.button`
 
   padding: 9px 16px;
   width: ${props => {
-    if (props.small) return '16px';
+    if (props.small) return "16px";
 
-    if (props.large) return '448px';
-    if (props.full) return '100%';
+    if (props.large) return "448px";
+    if (props.full) return "100%";
   }};
   opacity: ${props => {
-    if (!props.solid) return '0.7';
+    if (!props.solid) return "0.7";
   }};
   border-radius: 4px;
   cursor: pointer;
@@ -69,20 +70,20 @@ export const StyledButton = styled.button`
 
     opacity: ${props => {
       if (props.disabled) return;
-      if (!props.solid) return '1';
+      if (!props.solid) return "1";
     }};
   }
 
   @media screen and (max-width: 448px) {
     width: ${props => {
-      if (props.small) return '16px';
-      if (props.large) return '224px';
-      if (props.full) return '100%';
+      if (props.small) return "16px";
+      if (props.large) return "224px";
+      if (props.full) return "100%";
     }};
   }
 
   border: ${props =>
-    props.solid || props.disabled ? 'none' : `1px solid ${colors.black}`};
+    props.solid || props.disabled ? "none" : `1px solid ${colors.black}`};
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   display: inline-block;
   position: relative;
@@ -90,7 +91,7 @@ export const StyledButton = styled.button`
   ${props => (props.shadow ? boxShadow : null)};
 `;
 
-export const StyledLinkButton = StyledButton.withComponent('a');
+export const StyledLinkButton = StyledButton.withComponent("a");
 
 const rotate360 = keyframes`
   from {
@@ -104,7 +105,7 @@ const rotate360 = keyframes`
 
 const LoadingIcon = styled(Icon)`
   color: inherit;
-  margin-right: ${props => (props.withText ? '8px' : null)};
+  margin-right: ${props => (props.withText ? "8px" : null)};
   animation: ${rotate360} 2s linear infinite;
   /* font-size: inherit; */
 `;
@@ -135,7 +136,7 @@ class Button extends Component {
           href={href}
           id={id}
         >
-          {loading && <LoadingIcon name={'refresh'} />}
+          {loading && <LoadingIcon name={"refresh"} />}
           {children}
           {icon && <Icon className={`fa fa-${icon} ${className}`} />}
         </StyledLinkButton>
@@ -146,7 +147,7 @@ class Button extends Component {
       <StyledButton {...this.props} disabled={disabled || loading} id={id}>
         {loading && (
           <LoadingWrapper>
-            <LoadingIcon name="refresh" withText={!!loadingText} />{' '}
+            <LoadingIcon name="refresh" withText={!!loadingText} />{" "}
             {loadingText}
           </LoadingWrapper>
         )}
