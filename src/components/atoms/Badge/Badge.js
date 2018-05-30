1ed90e0f23b7c8c228953c53c0230d74fb7bb6c1
Morgan Faget <morgan.faget@capitalontap.com>
2018-05-30 13:53:09 +0100

    Adding the menu dropdown

---
+++ src/components/atoms/Badge/Badge.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledBadge = styled.div`
+  height: ${props => props.size + 'px'};
+  width: ${props => props.size + 'px'};
+  line-height: ${props => props.size + 'px'};
+  font-size: ${props => props.size / 2 + 'px'};
+  text-align: center;
+  border-radius: ${props => props.size + 'px'};
+  background: ${props => props.background};
+  color: ${props => props.color};
+`;
+
+const Badge = ({ content, ...props }) => {
+  return <StyledBadge {...props}>{content}</StyledBadge>;
+};
+
+Badge.PropTypes = {
+  content: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+  background: PropTypes.string
+};
+
+Badge.defaultProps = {
+  size: 48,
+  color: 'white',
+  background: '#27b161'
+};
+
+export default Badge;
