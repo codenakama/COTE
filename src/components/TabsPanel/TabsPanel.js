617073377c03bed663d1c80b971b51b34f81ea44
Tabasom Aryamanesh <tabasom.aryamanesh@capitalontap.com>
2018-06-05 17:51:14 +0100

    applying pr comments

--- src/components/TabsPanel/TabsPanel.js
+++ src/components/TabsPanel/TabsPanel.js
@@ -11,14 +11,15 @@ const ItemLink = styled.a`
   text-decoration: none;
   padding-bottom: 10px;
   color: #a4aab3;
-  :hover,
-  :active,
-  :focus {
+  :hover {
     color: #061f33;
     cursor: pointer;
     border-bottom: 1px solid #27b161;
     font-weight: bold;
   }
+  border-bottom: ${props => (props.isActive ? "1px solid  #27b161" : null)};
+  font-weight: ${props => (props.isActive ? "bold" : "normal")};
+  color: ${props => (props.isActive ? "#061f33" : "#a4aab3")};
 `;
 
 const ShowContentBox = styled.div`
@@ -47,8 +48,11 @@ export class Tabs extends Component {
     let tab = this.props.children[key];
 
     return (
-      <Item key={key} className={this.state.activeIndex == key ? "active" : ""}>
-        <ItemLink href="#" onClick={e => this.handleOnClick(key)}>
+      <Item key={key}>
+        <ItemLink
+          isActive={this.state.activeIndex === key}
+          onClick={e => this.handleOnClick(key)}
+        >
           {tab.props.title}
         </ItemLink>
       </Item>
@@ -65,11 +69,11 @@ export class Tabs extends Component {
     });
 
     return (
-      <div className={this.props.className}>
-        <ul className="tabs-nav">
+      <div>
+        <ul>
           {Object.keys(this.props.children).map(this.renderNavItem.bind(this))}
         </ul>
-        <div className="tabs-content">{tabs}</div>
+        <div>{tabs}</div>
       </div>
     );
   }
@@ -77,8 +81,7 @@ export class Tabs extends Component {
 
 export class Tab extends Component {
   render() {
-    const isActive = this.props.active;
-    if (isActive) {
+    if (this.props.active) {
       return <ShowContentBox>{this.props.children}</ShowContentBox>;
     } else {
       return <HideContentBox>{this.props.children}</HideContentBox>;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as initGlobalStyles } from './styles/injectGlobalStyles';
+export { default as initGlobalStyles } from "./styles/injectGlobalStyles";
 export {
   Heading1,
   Heading2,
@@ -7,38 +7,38 @@ export {
   Subtitle,
   Label,
   Caption
-} from './components/atoms/Typography';
-export { default as Button } from './components/atoms/Button/Button';
-export { default as Icon } from './components/atoms/Icon/Icon';
-export { default as ButtonGroup } from './components/ButtonGroup/ButtonGroup';
-export { default as Checkbox } from './components/Checkbox/Checkbox';
-export { default as CotCard } from './components/CotCard/CotCard';
-export { default as Dropdown } from './components/Dropdown/Dropdown';
+} from "./components/atoms/Typography";
+export { default as Button } from "./components/atoms/Button/Button";
+export { default as Icon } from "./components/atoms/Icon/Icon";
+export { default as ButtonGroup } from "./components/ButtonGroup/ButtonGroup";
+export { default as Checkbox } from "./components/Checkbox/Checkbox";
+export { default as CotCard } from "./components/CotCard/CotCard";
+export { default as Dropdown } from "./components/Dropdown/Dropdown";
 export {
   default as HighlightsBar
-} from './components/HighlightsBar/HighlightsBar';
-export { default as MessageCard } from './components/MessageCard/MessageCard';
-export { default as Paper } from './components/Paper/Paper';
-export { default as ProgressBar } from './components/ProgressBar/ProgressBar';
-export { default as ResultsList } from './components/ResultsList/ResultsList';
+} from "./components/HighlightsBar/HighlightsBar";
+export { default as MessageCard } from "./components/MessageCard/MessageCard";
+export { default as Paper } from "./components/Paper/Paper";
+export { default as ProgressBar } from "./components/ProgressBar/ProgressBar";
+export { default as ResultsList } from "./components/ResultsList/ResultsList";
 export {
   default as SearchTextInput
-} from './components/SearchTextInput/SearchTextInput';
-export { default as TextInput } from './components/TextInput/TextInput';
-export { default as StyledInput } from './components/TextInput/TextInput';
-export { default as Uploader } from './components/Uploader/Uploader';
-export { default as Widget } from './components/Widget/Widget';
-export { default as Navigation } from './components/Navigation/Navigation';
-export { default as ContentBox } from './components/ContentBox/ContentBox';
-export { default as WidgetBox } from './components/WidgetBox/WidgetBox';
+} from "./components/SearchTextInput/SearchTextInput";
+export { default as TextInput } from "./components/TextInput/TextInput";
+export { default as StyledInput } from "./components/TextInput/TextInput";
+export { default as Uploader } from "./components/Uploader/Uploader";
+export { default as Widget } from "./components/Widget/Widget";
+export { default as Navigation } from "./components/Navigation/Navigation";
+export { default as ContentBox } from "./components/ContentBox/ContentBox";
+export { default as WidgetBox } from "./components/WidgetBox/WidgetBox";
 export {
   default as HighlightsBarSlider
-} from './components/HighlightsBarSlider/HighlightsBarSlider';
+} from "./components/HighlightsBarSlider/HighlightsBarSlider";
 
-export { default as Tooltip } from './components/Tooltip/Tooltip';
-export { default as Toast } from './components/Toast/Toast';
-export { default as SideNav } from './components/SideNav/';
+export { default as Tooltip } from "./components/Tooltip/Tooltip";
+export { default as Toast } from "./components/Toast/Toast";
+export { default as SideNav } from "./components/SideNav/";
 export {
   default as MenuDropdown
-} from './components/MenuDropdown/MenuDropdown';
-export { default as TabsPanel } from './components/TabsPanel/';
+} from "./components/MenuDropdown/MenuDropdown";
+export { default as TabsPanel } from "./components/TabsPanel/TabsPanel";
