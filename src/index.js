c3317857b68d51c393658d40d939ccf73e4c04d7
Ricardo Abreu <rj.abreu89@gmail.com>
2018-06-11 13:28:28 +0100

    export togglebutton and radiobutton

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
-export { default as initGlobalStyles } from "./styles/injectGlobalStyles";
+export { default as Button } from "./components/atoms/Button/Button";
+export { default as Icon } from "./components/atoms/Icon/Icon";
 export {
+  Caption,
   Heading1,
   Heading2,
   Heading3,
-  Title,
-  Subtitle,
   Label,
-  Caption
+  Subtitle,
+  Title
 } from "./components/atoms/Typography";
-export { default as Button } from "./components/atoms/Button/Button";
-export { default as Icon } from "./components/atoms/Icon/Icon";
 export { default as ButtonGroup } from "./components/ButtonGroup/ButtonGroup";
 export { default as Checkbox } from "./components/Checkbox/Checkbox";
+export { default as ContentBox } from "./components/ContentBox/ContentBox";
 export { default as CotCard } from "./components/CotCard/CotCard";
 export { default as Dropdown } from "./components/Dropdown/Dropdown";
 export {
   default as HighlightsBar
 } from "./components/HighlightsBar/HighlightsBar";
+export {
+  default as HighlightsBarSlider
+} from "./components/HighlightsBarSlider/HighlightsBarSlider";
+export { default as HighlightStrip } from "./components/HighLightStrip/index";
+export {
+  default as MenuDropdown
+} from "./components/MenuDropdown/MenuDropdown";
 export { default as MessageCard } from "./components/MessageCard/MessageCard";
+export { default as Navigation } from "./components/Navigation/Navigation";
 export { default as Paper } from "./components/Paper/Paper";
 export { default as ProgressBar } from "./components/ProgressBar/ProgressBar";
+export { default as RadioButton } from "./components/RadioButton";
 export { default as ResultsList } from "./components/ResultsList/ResultsList";
 export {
   default as SearchTextInput
 } from "./components/SearchTextInput/SearchTextInput";
-export { default as TextInput } from "./components/TextInput/TextInput";
-export { default as StyledInput } from "./components/TextInput/TextInput";
-export { default as Uploader } from "./components/Uploader/Uploader";
-export { default as Widget } from "./components/Widget/Widget";
-export { default as Navigation } from "./components/Navigation/Navigation";
-export { default as ContentBox } from "./components/ContentBox/ContentBox";
-export { default as WidgetBox } from "./components/WidgetBox/WidgetBox";
-export { default as HighlightStrip } from "./components/HighLightStrip/index";
-export { default as UspList } from "./components/UspList/index";
-export {
-  default as HighlightsBarSlider
-} from "./components/HighlightsBarSlider/HighlightsBarSlider";
-
-export { default as Tooltip } from "./components/Tooltip/Tooltip";
-export { default as Toast } from "./components/Toast/Toast";
 export { default as SideNav } from "./components/SideNav/";
-export {
-  default as MenuDropdown
-} from "./components/MenuDropdown/MenuDropdown";
-export { default as Tabs } from "./components/Tabs/Tabs";
 export { default as Tab } from "./components/Tabs/Tab";
+export { default as Tabs } from "./components/Tabs/Tabs";
+export {
+  default as StyledInput,
+  default as TextInput
+} from "./components/TextInput/TextInput";
+export { default as Toast } from "./components/Toast/Toast";
+export { default as ToggleButton } from "./components/ToggleButton";
+export { default as Tooltip } from "./components/Tooltip/Tooltip";
+export { default as Uploader } from "./components/Uploader/Uploader";
+export { default as UspList } from "./components/UspList/index";
+export { default as Widget } from "./components/Widget/Widget";
+export { default as WidgetBox } from "./components/WidgetBox/WidgetBox";
+export { default as initGlobalStyles } from "./styles/injectGlobalStyles";
