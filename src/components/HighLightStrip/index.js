ea8aa5cdb7a4511b8534de916b6e76371d08466a
Ricardo Abreu <rj.abreu89@gmail.com>
2018-06-13 19:32:15 +0100

    update highlightstrip color opacity

--- src/components/HighLightStrip/index.js
+++ src/components/HighLightStrip/index.js
@@ -8,8 +8,8 @@ const Wrapper = styled.div`
   padding: 16px;
   background: ${props =>
     props.theme
-      ? hexToRgbA(props.theme.colorPrimary, 0.4)
-      : hexToRgbA(colors.primary, 0.4)};
+      ? hexToRgbA(props.theme.colorPrimary, 0.1)
+      : hexToRgbA(colors.primary, 0.1)};
 `;
 
 const HighlightStrip = ({ children, ...props }) => {
