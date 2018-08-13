6cae15c2f4a23e6d876b6244862071f96c5bfe82
Ricardo Abreu <rj.abreu89@gmail.com>
2018-08-13 14:41:28 +0100

    remove min height and min width

--- src/components/Paper/Paper.js
+++ src/components/Paper/Paper.js
@@ -1,13 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export const PaperWrapper = styled.div`
-  min-width: 64px;
-  min-height: 100px;
   border-radius: 4px;
   background-color: #ffffff;
   box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14);
-  padding: ${props => (props.padding ? props.padding : "1rem")};
+  padding: ${props => (props.padding ? props.padding : '1rem')};
 `;
 
 const Paper = props => {
