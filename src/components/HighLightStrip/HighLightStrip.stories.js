415357359d52971a518d3eb6f56562c49fec310d
Ricardo Abreu <rj.abreu89@gmail.com>
2018-08-13 12:35:05 +0100

    add danger prop

--- src/components/HighLightStrip/HighLightStrip.stories.js
+++ src/components/HighLightStrip/HighLightStrip.stories.js
@@ -1,10 +1,17 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import HighlightStrip from ".";
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import HighlightStrip from '.';
 
-storiesOf("HighlightStrip", module).add("With text inside", () => (
-  <HighlightStrip>
-    Pariatur magna dolore nulla enim excepteur tempor qui pariatur Lorem sit
-    velit.
-  </HighlightStrip>
-));
+storiesOf('HighlightStrip', module)
+  .add('With text inside', () => (
+    <HighlightStrip>
+      Pariatur magna dolore nulla enim excepteur tempor qui pariatur Lorem sit
+      velit.
+    </HighlightStrip>
+  ))
+  .add('With error message', () => (
+    <HighlightStrip danger>
+      Pariatur magna dolore nulla enim excepteur tempor qui pariatur Lorem sit
+      velit.
+    </HighlightStrip>
+  ));
