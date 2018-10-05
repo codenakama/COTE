fe042185054e5db4d2f4b69c7ea0d8618b258065
Ricardo Abreu <rj.abreu89@gmail.com>
2018-10-05 20:33:03 +0100

    added expansion panel

--- src/components/ExpansionPanel/expansionPanel.stories.js
+++ src/components/ExpansionPanel/expansionPanel.stories.js
@@ -1,9 +1,15 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import ScrollableContainer from '../atoms/ScrollBar';
 import ExpansionPanel from './index';
 
-storiesOf('ExpansionPanel', module).add('With content', () => (
-  <div>
+// const ScrollableScontainer = styled.div`
+//   overflow: hidden;
+//   max-height: 600px;
+// `;
+
+storiesOf('ExpansionPanel', module)
+  .add('Single with content', () => (
     <ExpansionPanel>
       <p>
         Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure qui
@@ -38,18 +44,37 @@ storiesOf('ExpansionPanel', module).add('With content', () => (
         exercitation sunt quis eiusmod fugiat nulla sunt laborum.
       </p>
     </ExpansionPanel>
+  ))
+  .add('Multiple with content', () => (
+    <div>
+      <ExpansionPanel>
+        <p>
+          Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure qui
+          excepteur consequat. In occaecat excepteur proident eiusmod. Lorem
+          deserunt nostrud consequat exercitation veniam non incididunt ipsum
+          est irure. Fugiat eiusmod consectetur commodo mollit ut non. Ex sit
+          mollit elit exercitation. Magna laborum nulla reprehenderit incididunt
+          nisi exercitation sunt quis eiusmod fugiat nulla sunt laborum.
+        </p>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <div style={{ padding: '1rem' }}>
+          <p>
+            Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure
+            qui excepteur consequat. In occaecat excepteur proident eiusmod.
+            Lorem deserunt nostrud consequat exercitation veniam non incididunt
+            ipsum est irure. Fugiat eiusmod consectetur commodo mollit ut non.
+            Ex sit mollit elit exercitation. Magna laborum nulla reprehenderit
+            incididunt nisi exercitation sunt quis eiusmod fugiat nulla sunt
+            laborum.
+          </p>
+        </div>
+      </ExpansionPanel>
+    </div>
+  ))
+  .add('Single with styled content', () => (
     <ExpansionPanel>
-      <p>
-        Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure qui
-        excepteur consequat. In occaecat excepteur proident eiusmod. Lorem
-        deserunt nostrud consequat exercitation veniam non incididunt ipsum est
-        irure. Fugiat eiusmod consectetur commodo mollit ut non. Ex sit mollit
-        elit exercitation. Magna laborum nulla reprehenderit incididunt nisi
-        exercitation sunt quis eiusmod fugiat nulla sunt laborum.
-      </p>
-    </ExpansionPanel>
-    <ExpansionPanel>
-      <div style={{ padding: '1rem' }}>
+      <div style={{ padding: '1rem', border: '1px solid red' }}>
         <p>
           Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure qui
           excepteur consequat. In occaecat excepteur proident eiusmod. Lorem
@@ -60,5 +85,47 @@ storiesOf('ExpansionPanel', module).add('With content', () => (
         </p>
       </div>
     </ExpansionPanel>
-  </div>
-));
+  ))
+  .add('Within scrollable container', () => (
+    <ScrollableContainer>
+      <ExpansionPanel>
+        <div style={{ padding: '1rem' }}>
+          <p>
+            Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure
+            qui excepteur consequat. In occaecat excepteur proident eiusmod.
+            Lorem deserunt nostrud consequat exercitation veniam non incididunt
+            ipsum est irure. Fugiat eiusmod consectetur commodo mollit ut non.
+            Ex sit mollit elit exercitation. Magna laborum nulla reprehenderit
+            incididunt nisi exercitation sunt quis eiusmod fugiat nulla sunt
+            laborum.
+          </p>
+        </div>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <div style={{ padding: '1rem' }}>
+          <p>
+            Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure
+            qui excepteur consequat. In occaecat excepteur proident eiusmod.
+            Lorem deserunt nostrud consequat exercitation veniam non incididunt
+            ipsum est irure. Fugiat eiusmod consectetur commodo mollit ut non.
+            Ex sit mollit elit exercitation. Magna laborum nulla reprehenderit
+            incididunt nisi exercitation sunt quis eiusmod fugiat nulla sunt
+            laborum.
+          </p>
+        </div>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <div style={{ padding: '1rem' }}>
+          <p>
+            Pariatur eiusmod quis eiusmod deserunt mollit cupidatat duis irure
+            qui excepteur consequat. In occaecat excepteur proident eiusmod.
+            Lorem deserunt nostrud consequat exercitation veniam non incididunt
+            ipsum est irure. Fugiat eiusmod consectetur commodo mollit ut non.
+            Ex sit mollit elit exercitation. Magna laborum nulla reprehenderit
+            incididunt nisi exercitation sunt quis eiusmod fugiat nulla sunt
+            laborum.
+          </p>
+        </div>
+      </ExpansionPanel>
+    </ScrollableContainer>
+  ));
