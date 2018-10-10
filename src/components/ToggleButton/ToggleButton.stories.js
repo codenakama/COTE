869c91059be53fbd31482d780c8873197bea3d31
Ricardo Abreu <rj.abreu89@gmail.com>
2018-10-10 14:23:50 +0100

    add disabled state

--- src/components/ToggleButton/ToggleButton.stories.js
+++ src/components/ToggleButton/ToggleButton.stories.js
@@ -1,21 +1,26 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import styled from "styled-components";
-import ToggleButton from ".";
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import styled from 'styled-components';
+import ToggleButton from '.';
 
 const Container = styled.div`
   max-width: 400px;
   margin: 0 auto;
 `;
 
-storiesOf("ToggleButton", module)
-  .add("Normal", () => (
+storiesOf('ToggleButton', module)
+  .add('Normal', () => (
     <Container>
       <ToggleButton />
     </Container>
   ))
-  .add("Controlled", () => (
+  .add('Controlled', () => (
     <Container>
       <ToggleButton isActive={true} />
+    </Container>
+  ))
+  .add('Is Disabled', () => (
+    <Container>
+      <ToggleButton isEnabled={false} disabled />
     </Container>
   ));
